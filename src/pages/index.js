9151fcc94edeccfc88faf63097644cf4bff809ec
Asad A. Malik <asadamalikk@gmail.com>
2018-11-24 02:22:37 +0500

    New Update

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import Image from '../components/image'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Starting the React</h1>
+    <p>My First Day of Learning the React. Really Excited about this. Now Watching the Videos </p>
+    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+export default IndexPage
